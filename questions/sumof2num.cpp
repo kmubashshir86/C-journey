5d2efaf7962b10a89ca91a904adac54457b6753b
kmubashshir86 <111277186+kmubashshir86@users.noreply.github.com>
2025-10-27 23:13:53 +0530

    Add files via upload

---
+++ questions/sumof2num.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"input no. 1:";
+    int x;
+    cin>>x;
+    cout<<"input no. 2:";
+    int y;
+    cin>>y;
+    cout<<"sum of "<<x<<" and "<<y<<" is "<<x+y;
+    return 0;
+    }
---
+++ questions/welcome.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"Print a welcome text in a seprate line:";
+    std::cout<<std::endl<<"------------------------------------------";
+    std::cout<<"\n"<<"welcome to"<<std::endl;
+    std::cout<<"w3resource.com"<<std::endl;
+    return 0;
+    }
