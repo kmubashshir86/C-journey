06cccfa659e6ae6f7f9d4c806ed3ef8b379ba45b
kmubashshir86 <111277186+kmubashshir86@users.noreply.github.com>
2025-10-30 00:39:14 +0530

    Add files via upload

---
+++ questions/Q6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char gender = 'f';
+	bool marriage_status =true;
+	short num = 2;
+	short year=2009;
+	int sallary=1500000;
+	double height=79.48;
+	float gpa=4.69;
+	long sall=12047235;
+	auto num2=995324987;
+	cout<<"The Gender is :"<<gender<<endl;
+	cout<<"Is she married? :"<<marriage_status<<endl;
+	cout<<"Number of sons she has :"<<num<<endl;
+	cout<<"Year of her appointment"<<year<<endl;
+	cout<<"Salary for a year:"<<sallary<<endl;
+	cout<<"Height is :"<<height<<endl;
+	cout<<"GPA is "<<gpa<<endl;
+	cout<<"Salary drawn upto:"<<sall<<endl;
+	cout<<"Balance till : "<<num2;
+	return 0;
+}
+
+
+
+
